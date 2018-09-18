8353d56e8bfd4c2b8390a9f007814cd2dd8a837b
hlavigne <hughes.la.vigne@usherbrooke.ca>
2018-09-18 16:03:12 -0400

    Removed safety switch

--- src/modules/px4iofirmware/px4io.c
+++ src/modules/px4iofirmware/px4io.c
@@ -313,7 +313,7 @@ user_start(int argc, char *argv[])
 #endif
 
 	/* start the safety switch handler */
-	safety_init();
+	// safety_init();
 
 	/* initialise the control inputs */
 	controls_init();
